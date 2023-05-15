c48a7af59e8b57e565295b6ab7c56c2c3aeda467
Porter Hatch <porterhatch1@gmail.com>
2023-05-15 14:28:58 -0600

    some minor additions and learning more about mvc

---
+++ Models/PostModel.cs
@@ -0,0 +1,11 @@
+
+
+public class PostModel
+{
+    public string content { get; set; }
+    public string imageUrl { get; set; }
+
+    private int userId { get; set; }
+
+
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,11 @@
+
+
+public class UserModel
+{
+    private int userId { get; set; }
+    private string username { get; set; }
+    private string password { get; set; }
+    private string email { get; set; }
+    private string profilePicture { get; set; }
+
+}
